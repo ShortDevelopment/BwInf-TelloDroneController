dc2b70009e2f448c955094539f911e9effebb006
Lukas Kurz <lukas@shortdev.de>
2022-02-11 17:53:24 +0100

    Remove control

--- TelloDroneControl/Program.cs
+++ TelloDroneControl/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TelloDroneControl
@@ -9,37 +10,44 @@ namespace TelloDroneControl
         static void Main(string[] args)
             => MainAsync(args).GetAwaiter().GetResult();
 
+        const int movement = 25;
+
         static async Task MainAsync(string[] args)
         {
-            Controller.Initialize();
+            Controller.Initialize(false);
 
             Controller.ResponseReceived += Controller_ResponseReceived;
             Controller.StatsReceived += Controller_StatsReceived;
 
+            Controller.SendCmd("battery?");
             Controller.SendCmd("takeoff");
-            await Task.Delay(2_000);
-
-            Controller.SendExpansionCmd("led 255 0 255");
-            Controller.SendExpansionCmd("mled sc");
-            await Task.Delay(2_000);
-            Controller.SendExpansionCmd("mled g rrrbb0ppp");            
-            Controller.SendCmd("cw 90");
-            await Task.Delay(2_000);
-            Controller.SendCmd("land");
 
             while (true)
             {
-                string cmd = Console.ReadLine();
-                if (cmd == "exit")
-                    return;
+                var key = Console.ReadKey().Key;
+                int horizontal = 0;
+                int horizontal2 = 0;
+                if (key == ConsoleKey.A)
+                    horizontal = -movement;
+                if (key == ConsoleKey.D)
+                    horizontal = movement;
 
-                Controller.SendCmd(cmd);
+                if (key == ConsoleKey.W)
+                    horizontal2 = movement;
+                if (key == ConsoleKey.S)
+                    horizontal2 = -movement;
+                Controller.SendCmd($"rc {horizontal} {horizontal2} 0 0");
+                //string cmd = Console.ReadLine();
+                //if (cmd == "exit")
+                //    return;
+
+                //Controller.SendCmd(cmd);
             }
         }
 
         private static void Controller_StatsReceived(TelloStats stats)
         {
-            
+
         }
 
         private static void Controller_ResponseReceived(string response)
---
+++ TelloDroneController.Uwp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using TelloDroneControl;
+using Windows.Gaming.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace TelloDroneController.Uwp
+{
+    public sealed partial class MainPage : Page
+    {
+        TelloController Controller = new TelloController();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+
+            Controller.Initialize(false);
+        }
+
+        private async void Gamepad_GamepadAdded(object sender, Gamepad pad)
+        {
+            await Task.Run(async () =>
+            {
+                pad.Vibration = new GamepadVibration()
+                {
+                    LeftMotor = 0.5,
+                    RightMotor = 0.8
+                };
+                await Task.Delay(1000);
+                pad.Vibration = new GamepadVibration();
+                while (true)
+                {
+                    var reading = pad.GetCurrentReading();
+                    Debug.Print(reading.LeftThumbstickX.ToString());
+                    await Task.Delay(100);
+                }
+            });
+        }
+    }
+}
